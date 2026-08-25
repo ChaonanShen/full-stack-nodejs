65e17d01cbe0befd6cf19daefda67b1aa0106d03
ChaonanShen <1686745041@qq.com>
2026-08-25 13:45:55 +0800

    part2: complete 2.7 (phonebook step2)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault() 
+    for (const person of persons) {
+      if (person.name === newName) {
+        alert(`${newName} is already added to phonebook`)
+        return
+      }
+    }
     setPersons(persons.concat({name: newName}))
     setNewName('')
   }
