51b287b6b2c76f1a293bf5063177b107caacae0e
ChaonanShen <1686745041@qq.com>
2026-08-27 14:27:00 +0800

    Complete exercise 3.2: phonebook info page

--- part3/phonebok-backend/index.js
+++ part3/phonebok-backend/index.js
@@ -30,6 +30,13 @@ app.get('/api/persons', (req, rsp) => {
     rsp.json(phonebooks)
 })
 
+app.get('/info', (req, rsp) => {
+    rsp.send(`
+        <p>Phonebook has info for ${phonebooks.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
